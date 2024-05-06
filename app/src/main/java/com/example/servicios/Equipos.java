d334215aed1197b1ac1c45a660223f62abd95891
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-05-06 16:42:35 -0500

    Equipos

--- app/src/main/java/com/example/servicios/Equipos.java
+++ app/src/main/java/com/example/servicios/Equipos.java
@@ -1,12 +1,19 @@
 package com.example.servicios;
 
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Equipos extends AppCompatActivity {
 
@@ -20,5 +27,34 @@ public class Equipos extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ListView lstEquipos = findViewById(R.id.lstEquipos);
+
+        String[] nombresEquipos = new String[] {"Real Madrid", "FC Barcelona", "Liverpool FC", "Paris Saint-Germain", "Bayern Munich"};
+        String[] paisesEquipos = new String[] {"España", "España", "Inglaterra", "Francia", "Alemania"};
+        String[] estadiosEquipos = new String[] {"Santiago Bernabéu", "Camp Nou", "Anfield", "Parc des Princes", "Allianz Arena"};
+        String[] capacidadesEstadios = new String[] {"81044", "99354", "54074", "48712", "75024"};
+        int[] imagenesEscudos = new int[] {R.drawable.real_madrid_escudo, R.drawable.barcelona_escudo, R.drawable.liverpool_escudo, R.drawable.psg_escudo, R.drawable.bayern_munich_escudo};
+
+        List<HashMap<String, String>> lista = new ArrayList<>();
+        for(int i = 0; i < nombresEquipos.length; i++) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("Equipo", nombresEquipos[i]);
+            map.put("Pais", paisesEquipos[i]);
+            map.put("Estadio", estadiosEquipos[i]);
+            map.put("Capacidad", capacidadesEstadios[i]);
+            map.put("Escudo", String.valueOf(imagenesEscudos[i]));
+            // Agregar más detalles aquí si es necesario
+            lista.add(map);
+        }
+
+        // Ahora usamos SimpleAdapter con nuestro adaptador personalizado
+        ListAdapter adaptador = new SimpleAdapter(this,
+                lista,
+                R.layout.item_equipo,
+                new String[]{"Equipo", "Pais", "Estadio", "Capacidad", "Escudo"}, // Añadimos la referencia al escudo
+                new int[]{R.id.txtEquipo, R.id.txtPais, R.id.txtEstadio, R.id.txtCapacidad, R.id.imgEscudo}); // Ahora también incluimos el ImageView
+
+        lstEquipos.setAdapter(adaptador);
     }
 }
